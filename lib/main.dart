4c52b6df998f5ed2db39acd024d79c99874fa787
https://github.com/tirth-hacker/FlutterApp <tirthypatel11@gmail.com>
2021-03-18 00:12:28 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:project/pages.dart';
+import 'package:project/Widgets/newTransaction.dart';
+import 'package:project/Widgets/transactionList.dart';
+import 'Models/transaction.dart';
+import 'Widgets/allMoths.dart';
+import 'Widgets/firstCard.dart';
+import 'Widgets/secondCard.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,201 +26,99 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  void changePage() {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => Pages()));
+  final List<Transaction> _userTransaction = [
+    // Transaction(
+    //   id: "1",
+    //   label: "shoes",
+    //   amount: "${200}",
+    //   icons: Icon(
+    //     Icons.shopping_cart_sharp,
+    //     color: Colors.red,
+    //     size: 40,
+    //   ),
+    //   dateTime: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: "2",
+    //   label: "Drinks",
+    //   amount: "${1500}",
+    //   icons: Icon(
+    //     Icons.no_drinks,
+    //     color: Colors.red,
+    //     size: 40,
+    //   ),
+    //   dateTime: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: "2",
+    //   label: "breakfast",
+    //   amount: "${1500}",
+    //   icons: Icon(
+    //     Icons.free_breakfast,
+    //     color: Colors.red,
+    //     size: 40,
+    //   ),
+    //   dateTime: DateTime.now(),
+    // ),
+  ];
+
+  void _addTransaction(String label, double amount) {
+    var addTx = Transaction(
+      label: label,
+      amount: amount.toString(),
+      dateTime: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+    setState(() {
+      _userTransaction.add(addTx);
+    });
+  }
+
+  void _addNewTransactionModel(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          child: NewTransaction(_addTransaction),
+          onTap: () {},
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _addNewTransactionModel(context),
+          ),
+        ],
         elevation: 5,
         iconTheme: IconThemeData(color: Colors.black),
         backgroundColor: Colors.white,
-        title: Center(
-          child: Text(
-            "DashBroad",
-            style: TextStyle(color: Colors.black),
-          ),
+        centerTitle: true,
+        title: Text(
+          "DashBroad",
+          style: TextStyle(color: Colors.black),
         ),
       ),
-      body: Column(
-        children: [
-          InkWell(
-            onTap: changePage,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              elevation: 5,
-              margin: EdgeInsets.only(top: 15, left: 12, right: 12, bottom: 12),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(5),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "OverView",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                          Icon(
-                            Icons.keyboard_arrow_right_outlined,
-                            size: 25,
-                          )
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Text(
-                      "Total expense",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                    Text(
-                      "\u{20B9}${15000}",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.red,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-                right: 12.0, left: 12, bottom: 15, top: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Feb 2021",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                InkWell(
-                  child: Text(
-                    "All Months",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            elevation: 5,
-            margin: EdgeInsets.only(right: 12, left: 12),
-            child: Container(
-              padding: EdgeInsets.all(15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Monthly Average",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Daily Expense",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16),
-                      ),
-                      Text(
-                        "Monthly Flow",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "\u{20B9}${12000}",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.red,
-                        ),
-                      ),
-                      Text(
-                        "\u{20B9}${1562}",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.red,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            margin: EdgeInsets.only(top: 20, left: 12, right: 12),
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Categories",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  ListTile(
-                    leading: Icon(
-                      Icons.no_drinks,
-                      color: Colors.red,
-                      size: 35,
-                    ),
-                    title: Text(
-                      "Friuts",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(
-                      "1 Transaction",
-                      style: TextStyle(
-                          color: Colors.green, fontWeight: FontWeight.bold),
-                    ),
-                    trailing: Text(
-                      "\u{20B9}${13200}",
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            FirstCard(),
+            AllMonths(),
+            SecondCard(),
+            TransactionList(_userTransaction),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _addNewTransactionModel(context),
+        child: Icon(Icons.add),
       ),
     );
   }
